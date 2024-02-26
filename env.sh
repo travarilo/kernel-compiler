10f391e12a0736c6b36191cddcdae73954282b0a
Rizky Benggolo <travarilo@gmail.com>
2024-02-26 18:06:30 +0800

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+KERNEL_SOURCE=https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/travarilo/kernel_xiaomi_courbet
+CLANG_SOURCE=https://bitbucket.org/bananadroid/clang-neutron/src/master
